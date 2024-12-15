9ab3b4402eff32b37e4c838847d6d8bc2b064ad3
VergilDsanji66 <mtvergil66@gmail.com>
2024-12-15 17:27:50 +0200

    Spawn and player damage

--- Scenes/Stage 1/game.gd
+++ Scenes/Stage 1/game.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+func spawn_mob():
+	var new_mob = preload("res://scripts/mobs/mobs.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()#generate random number function
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+
+func _on_timer_timeout() -> void:
+	spawn_mob()
--- scripts/mobs/mobs.gd
+++ scripts/mobs/mobs.gd
@@ -7,9 +7,8 @@ extends CharacterBody2D
 #@onready var mob_death_sfx: AudioStreamPlayer2D = $MobDeathSFX
 @onready var sprite = $AnimatedSprite2D  # Adjust path if necessary
 var player  # Reference to the player or target node
-var min_distance = 20.0  # Minimum distance before stopping
 var move_speed = 100.0  # Movement speed of the enemy
-var health = 20
+var health = 5
 
 func _ready():
 	# Set the initial animation
@@ -21,11 +20,9 @@ func take_damage():
 	gun_shot.play()
 	health -= 1
 	if health == 0:
-		game_manager.add_point()     #label for score counter
-		animation_player.play("mobdeath")  #sound when mobs die
-		#queue_free()
+		queue_free()
 	else:
-		sprite.play("hurt")
+		$AnimatedSprite2D.play("mobHurt")
 
 func switch_to_walk():
 	sprite.play("walk")
@@ -35,15 +32,9 @@ func _physics_process(delta: float) -> void:
 		# Calculate direction and distance to the player
 		var direction = global_position.direction_to(player.global_position)
 		var distance = global_position.distance_to(player.global_position)
-
-		if distance > min_distance:
-			# Move towards the player if farther than min_distance
-			velocity = direction * move_speed
-			sprite.play("walk")
-		else:
-			# Stop moving if too close
-			velocity = Vector2.ZERO
-			sprite.play("hurt")  # Optional: Play an idle or "stuck" animation
+		# Move towards the player if farther than min_distance
+		velocity = direction * move_speed
+		sprite.play("walk")
 
 		# Flip the sprite based on movement direction
 		if direction.x > 0:  # Moving right
@@ -53,13 +44,3 @@ func _physics_process(delta: float) -> void:
 
 		# Apply movement
 		move_and_slide()
-
-func _on_body_entered(body: Node2D) -> void:
-	  #How many mobs where killed
-	
-	if body == player:  # Check if the body is the player
-		if player.has_method("take_damage"):
-			player.take_damage()  # Inflict damage on the player
-	else:
-		if body.has_method("take_damage"):
-			body.take_damage()  # Damage other objects if needed
